80d9086899f09fb368631b67d944423a06381bb5
luiseduardovb <le_velasquez@yahoo.com>
2020-08-09 17:36:07 +0300

    added SignupModal

--- src/components/buttons/SignupButton.js
+++ src/components/buttons/SignupButton.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Components
 import { AuthButtonStyled } from "./styles";
+import SignupModal from "../modals/SignupModal";
 
 const SignupButton = () => {
-  return <AuthButtonStyled />;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsOpen(true);
+
+  return (
+    <>
+      <AuthButtonStyled onClick={openModal}>Sign Up</AuthButtonStyled>
+      <SignupModal isOpen={isOpen} closeModal={closeModal} />
+    </>
+  );
 };
 
 export default SignupButton;
---
+++ src/components/modals/SignupModal.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { observer } from "mobx-react";
+
+const SignupModal = ({ isOpen, closeModal }) => {
+  return;
+};
+
+export default observer(SignupModal);
